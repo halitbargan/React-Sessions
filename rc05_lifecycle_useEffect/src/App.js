cc5baac5945218753cce2496c519894f398cd2a1
halitbargan <hltbrgn@gmail.com>
2022-03-26 12:51:37 +0200

    add react lifecycle starter folder

---
+++ rc05_lifecycle_useEffect/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ClassComponent from './components/ClassComponent'
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h2>LifeCycles</h2> */}
+      <ClassComponent />
+    </div>
+  )
+}
+
+export default App
---
+++ rc05_lifecycle_useEffect/src/components/ClassComponent.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+class ClassComponent extends React.Component {
+  // constructor(props) {
+  //   super(props)
+  //   this.state = { count: 0 }
+  // }
+  state = { count: 0 }
+
+  increase = () => this.setState({ count: this.state.count + 1 })
+
+  // increase = () => this.setState({ name: 'Felix' })
+
+  //* component oluştuktan sonra gerçekleşecek eylem
+  componentDidMount() {
+    console.log('Class Comp Mounted')
+    // this.increase()
+  }
+
+  render() {
+    console.log('Class Comp Rendered!')
+    return (
+      <div className="class">
+        <h2>Class Component</h2>
+        <h3>Count: {this.state.count}</h3>
+        {/* <h3>Name: {this.state.name}</h3> */}
+        <button onClick={this.increase}>Increase</button>
+      </div>
+    )
+  }
+}
+
+export default ClassComponent
+
+
