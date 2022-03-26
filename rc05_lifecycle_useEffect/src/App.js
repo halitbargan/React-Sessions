a48972644c6a1b4d00d9540630499095fd41a8ac
halitbargan <hltbrgn@gmail.com>
2022-03-26 13:45:29 +0200

    useState completed

--- rc05_lifecycle_useEffect/src/App.js
+++ rc05_lifecycle_useEffect/src/App.js
@@ -1,13 +1,15 @@
 import React, {useState} from 'react'
 import ClassComponent from './components/ClassComponent'
+import UseStateComp from './components/UseStateComp'
 
 function App() {
   const [isVisible, setIsVisible]= useState(true)
   return (
     <div className="App">
       {/* <h2>LifeCycles</h2> */}
-      <button onClick={() => setIsVisible(!isVisible)}>Toggle Comp</button>
-      {isVisible && <ClassComponent />}
+      {/* <button onClick={() => setIsVisible(!isVisible)}>Toggle Comp</button>
+      {isVisible && <ClassComponent />} */}
+      <UseStateComp/>
       
     </div>
   )
--- rc05_lifecycle_useEffect/src/components/UseStateComp.js
+++ rc05_lifecycle_useEffect/src/components/UseStateComp.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import { useState } from "react"
 
 const UseStateComp = () => {
+  const [count, setCount]  = useState(0)
+  const [age, setAge] = useState(4)
+  const [user, setUser] = useState({name: "Felix", age:"25", email:"felix@gmail.com"})
+
+  const increase = () => setCount(count+1)
+  const increaseAge = () => setAge(age+1)
+  const updateUser = () => setUser({...user,name: "Halit"})
+  console.log(user);
+  
+
+
+
+
+
+
+
+
   return (
     <div className="function">
       <h2>Functional Component</h2>
+      <p>Count: {count}</p>
+     <button onClick={increase}>Increase Count</button>
+     <p>Age: {age}</p>
+     <button onClick={increaseAge}>Increase Age</button>
+     <p>Name: {user.name}</p>
+     <p>Age: {user.age}</p>
+     <p>Email: {user.email}</p>
+     <button onClick={updateUser}>Update User</button>
+     
     </div>
   )
 }
