b53f12add9102ce85240740811de278d4dcb580b
halitbargan <hltbrgn@gmail.com>
2022-03-31 23:29:35 +0300

    v2

--- recap/src/App.js
+++ recap/src/App.js
@@ -1,9 +1,29 @@
+// import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
+import axios from 'axios';
+import ProductItem from './components/Product/productItem';
+import './App.css';
 function App() {
+  //     okuma     yazma
+  const [products, setProducts] = useState([])
+  useEffect(()=>{
+    axios.get('https://fakestoreapi.com/products').then(({data})=>{
+      console.log("nasılsın", data)
+      setProducts(data)
+    })
+    // const {data} = await axios.get('https://fakestoreapi.com/products')
+    // console.log("naber", data)
+  }, [])
   return (
-    <div>
-      <h1>Merhaba Clarusway</h1>
+    <div className="App">
+      {products.map((osman) =>
+      <>
+        <ProductItem
+          ahmet={osman}
+        />
+        </>
+      )}
     </div>
   );
 }
-
 export default App;
