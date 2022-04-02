442097321ffeb5225c8b98c14d19e642faf1bdf7
halitbargan <hltbrgn@gmail.com>
2022-04-02 13:01:06 +0300

    curd 1

---
+++ 005-Task-Tracker-JSON-Server/src/App.js
@@ -0,0 +1,104 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import AddTask from "./components/AddTask";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const [showAddTask, setShowAddTask] = useState(false);
+
+  const baseUrl = "http://localhost:5000/tasks";
+
+  //* CRUD Create Read Update Delete
+
+  //* Fetch tasks
+  //  const fetchTasks = async()=>{
+  //   try {const res = await fetch (baseUrl);
+  //   const data = await res.json();
+  //   // console.log(data);
+  //   setTasks(data);
+  //   }catch (err) {
+  //     console.log(err)
+  //   }
+  // };
+
+  //* Fetch Tasks with axios
+  const fetchTasks = async ()=>{
+    // const res = await axios.get(baseUrl);
+    const { data } = await axios.get(baseUrl);
+    setTasks(data);
+  // console.log(data);
+  };
+  useEffect(() => {
+  fetchTasks();
+  }, []);
+  
+  // DELETE TASK
+  const deleteTask = (deletedTaskId) => {
+    // console.log("delete Task", deletedTaskId);
+    setTasks(tasks.filter((task) => task.id !== deletedTaskId));
+  };
+
+  //* ADD TASK
+  // const addTask = async(newTask)=>{
+  //   const res = await fetch(baseUrl, {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type" : "application/json",
+  //     },
+  //     body: JSON.stringify(newTask),
+  //   });
+  //   await res.json();
+  //   console.log(res);
+  //   fetchTasks();
+  // };
+
+  //* Add tasks with axios
+  const addTask = async (newTask)=>{
+    const res= await axios.post(baseUrl, newTask);
+    console.log(res);
+    fetchTasks();
+  }
+
+  // const addTask = (newTask) => {
+  //   const id = Math.floor(Math.random() * 1000 + 1);
+  //   const addNewTask = { id, ...newTask };
+  //   setTasks([...tasks, addNewTask]);
+  // };
+
+  // TOGGLE DONE
+  const toggleDone = (toggleDoneId) => {
+    // console.log("double click", toggleDoneId);
+    setTasks(
+      tasks.map((task) =>
+        task.id === toggleDoneId ? { ...task, isDone: !task.isDone } : task
+      )
+    );
+  };
+
+  // TOGGLESHOW
+  const toggleShow = () => setShowAddTask(!showAddTask);
+
+  return (
+    <div className="container">
+      <Header
+        title="TASK TRACKER"
+        showAddTask={showAddTask}
+        toggleShow={toggleShow}
+      />
+
+      {showAddTask && <AddTask addTask={addTask} />}
+
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} deleteTask={deleteTask} toggleDone={toggleDone} />
+      ) : (
+        <h2 style={{ textAlign: "center" }}>NO TASK TO SHOW</h2>
+      )}
+    </div>
+  );
+}
+
+export default App;
