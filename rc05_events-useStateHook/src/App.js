2b4b7c433cc5e0d7d6b70642c1594d2b03f914ae
halitbargan <hltbrgn@gmail.com>
2022-11-17 01:49:37 +0300

    class components

--- rc05_events-useStateHook/src/App.js
+++ rc05_events-useStateHook/src/App.js
@@ -1,10 +1,12 @@
 import Events from "./components/1-event-intro/Events";
+import Counter from "./components/2-classComponents/Counter";
 
 
 function App() {
   return (
     <div>
       <Events />
+      <Counter/>
     </div>
   );
 }
