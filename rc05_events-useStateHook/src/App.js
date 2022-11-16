5d11af87116b8d6fe92b5f075c6b6951e565f162
halitbargan <hltbrgn@gmail.com>
2022-11-17 01:07:32 +0300

    events

--- rc05_events-useStateHook/src/App.js
+++ rc05_events-useStateHook/src/App.js
@@ -1,14 +1,10 @@
-import Events from './components/1-event-intro/Events';
-import Counter from './components/2-classComponents/Counter';
-import UseStateExample from './components/3-useStateHook/UseStateExample';
+import Events from "./components/1-event-intro/Events";
+
 
 function App() {
   return (
     <div>
       <Events />
-      <Counter count={10}/>
-      <Counter />
-      <UseStateExample/>
     </div>
   );
 }
