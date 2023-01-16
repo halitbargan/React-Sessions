516f657472259a6cf0e72f1e6f4cc63899790476
halitbargan <hltbrgn@gmail.com>
2023-01-17 01:04:10 +0300

    FormObject

--- rc06_forms_otherEvents/src/App.js
+++ rc06_forms_otherEvents/src/App.js
@@ -1,4 +1,5 @@
 import Form from './components/1-forms/Form';
+import FormObject from './components/1-forms/FormObject';
 import KeyboardEvents from './components/2-keyboardClipboardEvents/keyboardClipboard.jsx';
 import MouseEvent from './components/3-mouseEvents/MouseEvent';
 
@@ -6,7 +7,8 @@ import MouseEvent from './components/3-mouseEvents/MouseEvent';
 function App() {
   return (
     <div className="container mt-4 text-center">
-      <Form />
+      {/* <Form /> */}
+      <FormObject />
       {/* <KeyboardEvents />
       <MouseEvent /> */}
     </div>
