17ec7dab85b7677a8d0ab943047b56e0662c5847
halitbargan <hltbrgn@gmail.com>
2023-01-25 00:20:56 +0300

    lifecycle

--- rc08_lifecycle-useEffect/src/components/LifeCycleMethods.jsx
+++ rc08_lifecycle-useEffect/src/components/LifeCycleMethods.jsx
@@ -34,14 +34,14 @@ class LifeCycleMethods extends React.Component {
     });
   };
 
-  //! 3-) Bir component DOM agacina eklendiginde calistirilir.
-  //! (İlk render sonrasi).
-  //! Her yasam dongusu icin bir kere calisir.
+  //? 3-) Bir component DOM agacina eklendiginde calistirilir.
+  //? (İlk render sonrasi).
+  //? Her yasam dongusu icin bir kere calisir.
   componentDidMount() {
     console.log('Mounted');
   }
-  //! 4-) Bu metot ilk render haric dige tüm render'lardan sonra cagrilir.
-  //!  prevState ve prevProps degerlerini parametre olarak alabilir.
+  //? 4-) Bu metot ilk render haric dige tüm render'lardan sonra cagrilir.
+  //?  prevState ve prevProps degerlerini parametre olarak alabilir.
   componentDidUpdate() {
     console.log('Updated');
   }
